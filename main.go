2426724c236c8b3397aabe5a6700b022cf3cac0e
JayJunor <Mvojunior237@gmail.com>
2026-02-24 12:19:28 +0100

    Basic repl loop

--- main.go
+++ main.go
@@ -1,40 +1,71 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"github.com/jayjunior/eval/internal/visitors"
+	"strings"
+
 	"github.com/jayjunior/eval/internal"
+	"github.com/jayjunior/eval/internal/visitors"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: eval <expression>")
-		os.Exit(1)
+	if len(os.Args) >= 2 {
+		evaluateExpression(os.Args[1], true)
+		return
 	}
 
-	expression := os.Args[1]
+	fmt.Fprintln(os.Stdout, "Welcome to the eval repl")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			break
+		}
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+		if strings.ToLower(line) == "exit" {
+			fmt.Println("Bye :)")
+			os.Exit(0)
+		}
+		evaluateExpression(line, false)
+	}
 
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+	}
+}
+
+func evaluateExpression(expression string, exitOnError bool) {
 	tokens, err := internal.Tokenize(expression)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Lexer error: %v\n", err)
-		os.Exit(1)
+		if exitOnError {
+			os.Exit(1)
+		}
+		return
 	}
 
-	ast, err := internal.Parse(tokens)
+	exprAst, err := internal.Parse(tokens)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Parser error: %v\n", err)
-		os.Exit(1)
+		if exitOnError {
+			os.Exit(1)
+		}
+		return
 	}
 
-	printer := visitors.CreatePrinter()
-	printer.PrintAST(ast)
-
 	evaluator := visitors.Evaluator{}
-	res, err := evaluator.Evaluate(ast)
+	res, err := evaluator.Evaluate(exprAst)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error evaluating the expression: %v\n", err)
-		os.Exit(1)
+		if exitOnError {
+			os.Exit(1)
+		}
+		return
 	}
 	fmt.Println(res)
 }
